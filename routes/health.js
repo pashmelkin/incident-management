bcbe7b7c89a25f98bb1595e6dd37b7adcc18fba5
Pavel Shmueli <Pavel.Shmueli@myob.com>
2019-10-08 12:25:34 +1100

    Add readyness probe and modify live probe

--- routes/health.js
+++ routes/health.js
@@ -3,9 +3,8 @@
 var express = require('express');
 var router = express.Router();
 
-
 router.get('/', function(req, res, next) {
-  res.send("alive");
+  res.send("Ok");
 });
 
 module.exports = router;
---
+++ routes/ready.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const express = require('express');
+const incidentService = require("../middleware/getOnCall");
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+    const team = 'IDAM';
+
+    incidentService.getOnCall(team, 'primary').then(function (user) {
+      if (user) {
+        res.send('Ok');
+    }
+    });
+});
+
+
+module.exports = router;
