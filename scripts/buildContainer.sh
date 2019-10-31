ed8282a5114c22da249b92cb52572ba94db2e1dc
Pavel Shmueli <Pavel.Shmueli@myob.com>
2019-10-31 16:30:58 +1100

    Initial commit

--- scripts/buildContainer.sh
+++ scripts/buildContainer.sh
@@ -1,4 +1,15 @@
-myob-auth l
+#!/usr/bin/env bash
+
+die() { echo "$1" >&2; exit 1; }
+
+hash aws 2>/dev/null    || die "missing aws!"
+hash docker 2>/dev/null || die "missing docker!"
+
+# check everything is turned on and that we're authenticated.
+docker info &>/dev/null                 || die "docker's not running"
+aws sts get-caller-identity &>/dev/null || die "missing aws auth, y'all"
+
+#aws_parameters=$(aws ssm get-parameters-by-path --path $incident-management --with-decryption --query 'Parameters[*].{Name:Name,Value:Value}')
 
 $(aws ecr get-login --no-include-email --region ap-southeast-2)
 docker build -t incident-management-docker-repository .
