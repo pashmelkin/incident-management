56ebc1d01de95e2ee8f9633359fbf1bbf66e9593
Pavel Shmueli <Pavel.Shmueli@myob.com>
2019-11-20 15:37:41 +1100

    Add mock endpoint for UI integration

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var reportRouter = require('./routes/report');
 var healthCheckRouter = require('./routes/health');
 var readyCheckRouter = require('./routes/ready');
 var contactRouter = require('./routes/contact');
+var mockRouter = require('./routes/mock');
 
 
 var app = express();
@@ -34,6 +35,7 @@ app.use('/report', reportRouter);
 app.use('/health', healthCheckRouter);
 app.use('/ready', readyCheckRouter);
 app.use('/contact', contactRouter);
+app.use('/mock', mockRouter);
 
 
 // catch 404 and forward to error handler
@@ -52,5 +54,13 @@ app.use(function(err, req, res) {
   res.render('error');
 });
 app.get('/favicon.ico', (req, res) => res.status(204));
+
+app.all('*', (req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, PATCH, OPTIONS');
+   res.header('Access-Control-Allow-Headers', '*');
+   res.header('Access-Control-Allow-Credentials', true);
+   next();
+ });
 
 module.exports = app;
